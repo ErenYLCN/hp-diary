715684e57b9dff8116d0e14449902a6b5b3874e2
Eren Yalçın <yalcineren97@gmail.com>
2025-05-18 15:40:40 +0300

    server: setup env.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,21 @@
 import express, { Request, Response } from "express";
+import { DB_USERNAME, DB_PASSWORD } from "./config/env";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
 const app = express();
-dotenv.config();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+
+
 mongoose
-  .connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@hp-diary-db.5fcu8zb.mongodb.net/?retryWrites=true&w=majority&appName=hp-diary-db`)
+  .connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@hp-diary-db.5fcu8zb.mongodb.net/?retryWrites=true&w=majority&appName=hp-diary-db`)
   .then(() => {
     console.log("Connected to MongoDB");
   })
