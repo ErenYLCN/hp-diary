b8d8ede6c1199a91c00db50b88feb9f37c2988e9
Eren Yalçın <yalcineren97@gmail.com>
2025-05-26 01:49:25 +0300

    client: setup `redux`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import api from "./services/api";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { increment } from "./store/slices/counterSlice";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     async function getUsers() {
@@ -32,7 +35,7 @@ function App() {
               </div>
               <h1 className="mb-8">Vite + React</h1>
               <div className="bg-white p-8 rounded-lg shadow-md mb-8">
-                <button className="btn btn--primary mb-4" onClick={() => setCount((count) => count + 1)}>
+                <button className="btn btn--primary mb-4" onClick={() => dispatch(increment())}>
                   count is {count}
                 </button>
                 <p className="text-secondary">
